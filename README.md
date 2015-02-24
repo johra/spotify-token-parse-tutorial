190c258a2dc8154c09f8cdb1f822bc4e26f0145d
Johan Ramström <johan.ramstrom@gmail.com>
2015-02-24 10:35:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Spotify Token Swap Service for Parse
+=============
+
+How to create your own Token Exchange Service using Parse. 
+The current CLIENT_ID, CLIENT_SECRET and CLIENT_CALLBACK_URL will work with Spotify example apps.
+
+Setup
+=============
+
+Create an account at www.parse.com and use the following guide to set up Cloud Code:
+https://parse.com/docs/cloud_code_guide
+
+Replace main.js with main.js in this repository
+
+Replace the credentials to yours
+
+Deploy to Parse Cloud Code
+
+Contiune the Spotify Tutorial:
+https://developer.spotify.com/technologies/spotify-ios-sdk/tutorial/#setting-up-your-token-exchange-service
---
+++ cloud/main.js
@@ -0,0 +1,125 @@
+var express = require("express"),
+    app = express(),
+    crypto = require('crypto'),
+    buffer = require('buffer'),
+    url = require('url');
+
+var config = new Parse.Object("Config");
+config.set("client_id", "e6695c6d22214e0f832006889566df9c");
+config.set("client_secret", "29eb02041ba646179a1189dccac112c7");
+config.set("callback_url", "spotifyiossdkexample://");
+config.set("endpoint", "https://accounts.spotify.com");
+
+var AUTH_HEADER = "Basic " + new buffer.Buffer(config.get("client_id") + ":" + config.get("client_secret")).toString("base64");
+
+app.use(express.bodyParser());
+
+app.post("/swap", function (req, res) {
+    if (!req.body || !req.body.hasOwnProperty("code")) {
+        res.status(550).send("Permission Denied");
+        return;
+    }
+
+    var form_data = {
+        "grant_type": "authorization_code",
+        "redirect_uri": config.get("callback_url"),
+        "code": req.body.code
+    };
+
+    Parse.Cloud.httpRequest({
+        method: "POST",
+        url: url.resolve(config.get("endpoint"), "/api/token"),
+        headers: {
+            "Authorization": AUTH_HEADER,
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: form_data,
+        success: function(httpResponse) {
+            if (httpResponse.status != 200) {
+                res.status(550).send("Permission Denied");
+                return;
+            }
+
+            var token_data = JSON.parse(httpResponse.text);
+
+            res.status(200).set({
+                "Content-Type": "application/json"
+            }).send(token_data);
+        },
+        error: function(httpResponse) {
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+    });
+});
+
+app.post("/refresh", function (req, res) {
+    if (!req.body || !req.body.hasOwnProperty("refresh_token")) {
+        res.status(550).send("Permission Denied");
+        return;
+    }
+
+    var refresh_token = req.body.refresh_token;
+
+    var form_data = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    };
+
+    Parse.Cloud.httpRequest({
+        method: "POST",
+        url: url.resolve(config.get("endpoint"), "/api/token"),
+        headers: {
+            "Authorization": AUTH_HEADER,
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: form_data,
+        success: function(httpResponse) {
+            if (httpResponse.status != 200) {
+                res.status(550).send("Permission Denied");
+                return;
+            }
+
+            res.status(httpResponse.status).set({
+                "Content-Type": "application/json"
+            }).send(httpResponse.text);
+
+        },
+        error: function(httpResponse) {
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+    });
+
+});
+
+app.post("/api/token", function (req, res) {
+
+    res.status(200).set({
+        "Content-Type": "application/json"
+    });
+
+    switch (req.body.grant_type) {
+        case "authorization_code": {
+            res.status(200).set({
+                "Content-Type": "application/json"
+            }).send({
+                "refresh_token": "REFRESH TOKEN"
+            });
+        } break;
+        case "refresh_token": {
+            res.status(200).set({
+                "Content-Type": "application/json"
+            }).send({
+                "access_token": "ACCESS TOKEN"
+            });
+        } break;
+        default: {
+            res.status(550).set({
+                "Content-Type": "text/html"
+            }).send("<html><body><h1>Access Denied</h1></body></html>");
+        } break;
+    }
+});
+
+app.listen();
